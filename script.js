3bb233f185f38cdd852673df64234e1d293c2c6a
saltedEgg3 <bryancstrtumazar@gmail.com>
2026-06-11 22:02:16 +0800

    Update Portfolio Design

--- script.js
+++ script.js
@@ -13,3 +13,58 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 
 });
+
+
+const texts = [
+    "Software Developer",
+    "Unity Developer",
+    "Computer Engineering Student",
+    "Blockchain Enthusiast"
+];
+
+let count = 0;
+
+function changeText() {
+
+    const typing = document.getElementById("typing");
+
+    if (typing) {
+
+        typing.style.opacity = 0;
+
+        setTimeout(() => {
+
+            typing.textContent = texts[count];
+
+            typing.style.opacity = 1;
+
+            count = (count + 1) % texts.length;
+
+        }, 300);
+
+    }
+
+}
+
+changeText();
+
+setInterval(changeText, 2500);
+
+
+window.addEventListener('scroll', () => {
+
+    const header = document.querySelector('header');
+
+    if (window.scrollY > 50) {
+
+        header.style.background = "rgba(15,23,42,0.85)";
+        header.style.backdropFilter = "blur(20px)";
+
+    } else {
+
+        header.style.background = "rgba(255,255,255,0.05)";
+        header.style.backdropFilter = "blur(15px)";
+
+    }
+
+});
